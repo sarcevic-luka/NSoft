08af27f3ba3eb8b948ab014581fe188db493ec26
Luka Sarcevic <lukasarcevic.business@gmail.com>
2021-08-31 17:44:16 +0200

    * Added Compositional layout vendors.

---
+++ NSoftJobAssignment/Common/UI/Collection view/Layout/CompositionalLayoutVendor.swift
@@ -0,0 +1,12 @@
+//
+//  CompositionalLayoutVendor.swift
+//  NSoftJobAssignment
+//
+//  Created by Luka Šarčević on 31.08.2021..
+//
+
+import Foundation
+
+public protocol CompositionalLayoutVendor {
+  func vendLayout() -> UICollectionViewCompositionalLayout
+}
